ddde7dce808c8187786caf583a497806a2564c2b
beatriz <108158611+bcanutoo@users.noreply.github.com>
2025-11-02 10:29:39 -0300

    classe quadrado
    Added a PrincipalQuadrado class to demonstrate Quadrado functionality.

--- ex02.java
+++ ex02.java
@@ -13,3 +13,20 @@ public class Quadrado {
         return lado * 4; 
     }
 }
+class PrincipalQuadrado {
+    public static void main(String[] args) {
+
+        Quadrado q1 = new Quadrado(2);
+        Quadrado q2 = new Quadrado(5.5);
+        Quadrado q3 = new Quadrado(7);
+
+        System.out.println("Área Q1: " + q1.area());
+        System.out.println("Perímetro Q1: " + q1.perimetro());
+
+        System.out.println("Área Q2: " + q2.area());
+        System.out.println("Perímetro Q2: " + q2.perimetro());
+
+        System.out.println("Área Q3: " + q3.area());
+        System.out.println("Perímetro Q3: " + q3.perimetro());
+    }
+}
